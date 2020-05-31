64a527c640c68e226b1b690f1d88da184b9849ce
kristal4ik <64975256+kristal4ik@users.noreply.github.com>
2020-05-31 17:55:39 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+//alert("ВЫ были взломаны!!!!")
+$(function () {
+	$(window).scroll(function() {
+	    $('#future .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.time').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+})
